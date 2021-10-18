f846cdcb8d28200f8f07d14afeaac85447cb3e60
Danny Perilla <djperillam@unal.edu.co>
2021-10-18 17:59:51 -0500

    Added over and under examples

---
+++ 2021-10-15-NumericalErrors-I/underoverflow.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+  float xover = 1.23e40;
+  float xunder = 1.23e-50;
+  std::cout << "xover = " << xover << "\n";
+  std::cout << "xunder = " << xunder << "\n";  
+  int m = 2500000000;
+  int n = 2000000000;
+  std::cout << "m = " << m << "\n";
+  std::cout << "n = " << n << "\n";
+  return 0;
+}
