7e89957fc8f8cd8bff60bc119d37b879d03ffb1f
Danny <djperillam@unal.edu.co>
2022-04-26 09:50:24 -0500

    archivos originales para hacer testing

---
+++ 2022-04-22-Assignment-UnitTesting/vector_ops.cpp
@@ -0,0 +1,5 @@
+#include "vector_ops.hpp"
+double mean(const std::vector<double> & data)
+{
+  ????
+}
---
+++ 2022-04-22-Assignment-UnitTesting/vector_test.cpp
@@ -0,0 +1,9 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch2/catch.hpp"
+
+#include "vector_ops.hpp"
+
+TEST_CASE( "Mean of a vector is computed", "[mean]" ) {
+    //REQUIRE( factorial(0) == 1 );
+  crear vectores de prueba y comprobar los resultados con REQUIRE
+}
